f37e605a629b9be015806d6574d9eb7d5474f9f6
Theosaccount <16leechowt89@millshighsch.org>
2015-08-18 09:19:54 -0700

    HellowWorld Program
    First day step by step on how to program

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * This will do a simple print line. Our first Java code as a class
+ * 
+ * @author (Theo) 
+ * @version (8-18-15)
+ */
+public class HelloWorld
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World! From Ted");
+    }
+
+}
